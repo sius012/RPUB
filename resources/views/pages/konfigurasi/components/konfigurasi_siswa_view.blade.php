5d29749e5dbd270e3c3f9f25899e46dc399bb2fc
Elia Manuel <122024564+eliamanuela@users.noreply.github.com>
2023-08-18 16:56:03 +0700

    konfigurasi, modal jurusan, angkatan, jenis, dan melanjutkan membaut card yang didalam file index.blade.php di folder konfigurasi

---
+++ resources/views/pages/konfigurasi/components/konfigurasi_siswa_view.blade.php
@@ -0,0 +1,8 @@
+<div class="row" id="container-siswa">
+    <div class="button">
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#basicModal"
+            id="tambah-jurusan">
+            Tambah Data Siswa
+        </button>
+    </div>
+</div>
---
+++ resources/views/pages/konfigurasi/components/modal_jenis.blade.php
@@ -0,0 +1,63 @@
+<!-- Modal Untuk Tambah Jurusan -->
+<div class="modal fade" id="modal-angkatan" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel1">Tambah Angkatan</h5>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col mb-3">
+            <label for="nameBasic" class="form-label">Nama Kategori</label>
+            <input type="text" name="nama_kategori"  class="form-control" placeholder="Masukkan nama kategori" />
+          </div>
+        </div>
+        <div class="row">
+            <div class="col mb-3">
+              <label for="nameBasic" class="form-label">Jurusan</label>
+              <select name="jurusan">
+                <option value="">jurusan ss</option>
+                <option>jurusan a</option>
+                <option>jurusan b</option>
+                <option>jurusan c</option>
+              </select>
+            </div>
+          </div>
+        <div class="row">
+            <div class="col mb-3">
+              <label for="nameBasic" class="form-label">Tipe</label>
+              <select name="jenis_projek">
+                <option value="">folder,tugas ss</option>
+                <option>folder,tugas a</option>
+                <option>folder,tugas b</option>
+                <option>folder,tugas c</option>
+              </select>
+            </div>
+          </div>
+      <div class="row g-2">
+        <div class="col mb-0">
+          <label for="emailBasic" class="form-label">Icon</label>
+          <input type="file" name="" class="form-control" placeholder="Masukkan icon"/>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+          Close
+        </button>
+        <button type="button" class="btn btn-primary">Buat Jenis Tugas</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+{{--  /modal tambah jenis  --}}
+
+
