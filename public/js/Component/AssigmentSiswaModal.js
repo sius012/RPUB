bc568bc08adb00197a4f2143d4ace8ba2e02164d
josuaebe <josuaebe13@gmail.com>
2023-08-23 16:52:10 +0700

    membuat AssigmentSiswamodal.js

---
+++ public/js/Component/AssigmentSiswaModal.js
@@ -0,0 +1,58 @@
+class AssigmentSiswaModal {
+    constructor(container){
+        this.container = container;
+        this.modal = new bootstrap.Modal(container);
+        this.siswaList;
+        this.id_jurusan;
+        this.projek;
+        this.tugas;
+        this.page_setup;
+        this.nama_component = "AssigmentSiswaModal";
+    }
+
+    init(projek, tugas){
+        this.id_jurusan = projek.id_jurusan;
+        this.projek = projek;
+        this.tugas = tugas;
+        this.container.find("input[name=id_projek]").val(this.projek.id);
+        this.container.find("input[name=id_tugas]").val(this.tugas.id);
+
+
+    }
+
+    loadContext(kw = null){
+        this.siswaList =
+            Siswa.byQuery({ id_jurusan: this.id_jurusan, nama: kw });
+
+        this.container.find(".siswa-container").html("");
+        this.siswaList.forEach(element => {
+            this.container.find(".siswa-container").append(`
+            <div class='row' data-id='${element.id}'>
+            <div class='col-10'>
+                 <h1>${element.id}</h1>
+            </div>
+            <div class='col-2>
+                 <button class='btn btn-primary tambah-button btn-tambah-siswa'></button>
+            </div>     
+            </div>
+             `);
+
+        });
+    }
+
+    show() {
+        this.modal.show();
+    }
+
+    globalEventListener(){
+        var ctx = this;
+        this.container.find(".input-siswa").kwyup(function(){
+            ctx.loadContext(null, $(this).val());
+        });
+        this.container.delegate(".btn-tambah-siswa", "click", function(){
+            
+        });
+    }
+
+    
+}  
